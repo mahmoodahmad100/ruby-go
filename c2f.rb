0ebfa87da85d3166846e90f48bfd7ab4386e5f28
mahmoodahmad100 <mahmood073@gmail.com>
2018-06-15 19:25:47 +0200

    the ablity to type the celsius value

--- c2f.rb
+++ c2f.rb
@@ -1,5 +1,6 @@
-celsius = 95
-fahrenheit = (celsius * 9 / 5) + 32
+print "type the celsius value: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
 print "The result is: "
 print fahrenheit
 puts "."
